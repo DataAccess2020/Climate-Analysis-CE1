dbd29c2b2f867016a30c9578d5d84964dacaec5d
catalinamasp <77728376+catalinamasp@users.noreply.github.com>
2021-01-23 18:09:04 +0100

    Before Filtering

---
+++ ce1.R
@@ -0,0 +1,12 @@
+library(rio)
+library(tidyverse)
+setwd("/Users/catalinamasp/Documents/UNIMI/R")
+ess <- import("ESS8e02.2_F1.sav")
+ess_pg <- ess[, c("sbsrnen","cntry")]
+ess_pg2 <- na.omit(ess_pg)
+ess_pg2 <-ess_pg2 %>%
+  mutate(recoded_opinion = ifelse(sbsrnen <= 2, "Favor",
+                                  ifelse(sbsrnen == 3, "Neutral",
+                                          "Against")))
+
+
