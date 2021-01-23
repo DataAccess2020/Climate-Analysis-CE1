fd454c1b85e84ddba9e2e6b1b736dad689971d6f
Yaming Dong <dym19960402@gmail.com>
2021-01-23 19:05:04 +0100

    change the selected data

--- ce1.R
+++ ce1.R
@@ -1,22 +1,27 @@
 library(rio)
 library(tidyverse)
-setwd("/Users/catalinamasp/Documents/UNIMI/R")
-ess <- import("ESS8e02.2_F1.sav")
+setwd("D:/COM/Data analysis/ESS8e02_1.stata")
+ess <- import("ESS8e02_1.dta")
 ess_pg <- ess[, c("sbsrnen","cntry")]
 ess_pg2 <- na.omit(ess_pg)
 ess_pg2 <-ess_pg2 %>%
   mutate(recoded_opinion = ifelse(sbsrnen <= 2, "Favor",
                                   ifelse(sbsrnen == 3, "Neutral",
                                           "Against")))
+ess_pg2 <- ess_pg2 %>%
+  mutate(region = ifelse(cntry == "ES" | cntry == "PT" | cntry =="IT", "Southern Countries", 
+  ifelse(cntry == "IS" | cntry == "SE" | cntry == "FI" | cntry == "NO", "Northern Countries", 
+  "Others" )))
+
 ess_pg2 <- filter(
   ess_pg2,
-  cntry %in% c("DE", "IT")
+  region %in% c("Southern Countries", "Northern Countries")
 )
-ess_pg2 %>% 
-  group_by(recoded_opinion, cntry)%>% 
+
+ess_pg2 %>%
+  group_by(recoded_opinion, region, cntry)%>% 
   count()%>% 
-ggplot(aes(x = recoded_opinion, y = n, color = cntry, fill = cntry))+
+ggplot(aes(x = recoded_opinion, y = n, color = region, fill = region))+
        geom_col()+
-  facet_wrap(~ cntry, scales = "free_y")
+  facet_wrap(~ region)
        theme_bw()  
-       
